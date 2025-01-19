ff8b191f3c51da5e8c52358954db34218d9f6bc9
Wandile Kubheka <retroff01@gmail.com>
2025-01-19 22:08:43 +0200

    application.properties functional

--- src/main/java/com/enviro/assessment/grad001/wkubheka023/dao/DatabaseAccess.java
+++ src/main/java/com/enviro/assessment/grad001/wkubheka023/dao/DatabaseAccess.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-public interface DatabaseAccess extends .<Waste, Integer> {
+public interface DatabaseAccess extends JpaRepository<Waste, Integer> {
 
 }
