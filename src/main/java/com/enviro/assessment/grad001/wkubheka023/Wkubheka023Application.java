21c1c4ce618cefa5ffc6b7f3003eccf0d4c00402
Wandile Kubheka <retroff01@gmail.com>
2025-01-19 14:24:01 +0200

    initial project

---
+++ src/main/java/com/enviro/assessment/grad001/wkubheka023/Wkubheka023Application.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.wkubheka023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wkubheka023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wkubheka023Application.class, args);
+	}
+
+}
